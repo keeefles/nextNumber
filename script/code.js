f491b9ba5f8b564216ac8fc5db9a9bd1a48aa294
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-27 11:21:00 +0200

    i dont know bro

--- script/code.js
+++ script/code.js
@@ -1,6 +1,24 @@
-document.getElementById('btnSubmit').addEventListener('click', function(){
+let currentIdx = 0;
 
-    let input = document.querySelector('[data-input]').value;
-    let nextNumber = input +1; document.querySelector('[data-output]').textContent = nextNumber;
+function nextNumber(numbers) {
+    if (currentIdx >= numbers.length) {
+        currentIdx = 0;
+    } const nextNumber = numbers[currentIdx];
+    currentIdx++;
 
-});
+return nextNumber;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    currentNumberElement = document.getElementById('currentNumber');
+
+   function updateNumber() {
+    const nextButton = nextNumber(array);
+    currentNumberElement.textContent = nextNumber;
+   } 
+    
+
+ updateNumber();
+btnSubmit.addEventListener('click', updateNumber);
+})
