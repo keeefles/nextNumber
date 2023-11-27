52985aabf7f9f10879c213282c461a0bb98fb227
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-27 15:10:44 +0200

    didnt finish

--- script/code.js
+++ script/code.js
@@ -1,24 +1,7 @@
-let currentIdx = 0;
+let array = [15, 28, 31, 41, 20, 66, 78, 84, 93, 62];
 
-function nextNumber(numbers) {
-    if (currentIdx >= numbers.length) {
-        currentIdx = 0;
-    } const nextNumber = numbers[currentIdx];
-    currentIdx++;
+function nextNumber() {
 
-return nextNumber;
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    currentNumberElement = document.getElementById('currentNumber');
-
-   function updateNumber() {
-    const nextButton = nextNumber(array);
-    currentNumberElement.textContent = nextNumber;
-   } 
-    
-
- updateNumber();
-btnSubmit.addEventListener('click', updateNumber);
-})
+let nextNumber = document.getElementById('btn').addEventListener('click', nextNumber);
